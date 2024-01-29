6303f4789c900a041be1ff5ee270877d67bfaf07
DiegoAndre Leffa <diegodocanto@gmail.com>
2024-01-29 18:51:21 -0300

    feat: create entity users

---
+++ src/entities/users/users.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", length: 100 })
+  email: string;
+
+  @Column({ type: "varchar", length: 50 })
+  username: string;
+
+  @Column({ type: "varchar", length: 50 })
+  password: string;
+
+  @Column({ type: "varchar", length: 50 })
+  first_name: string;
+
+  @Column({ type: "varchar", length: 50 })
+  last_name: string;
+}
